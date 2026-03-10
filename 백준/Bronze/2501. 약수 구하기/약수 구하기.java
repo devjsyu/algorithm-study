2c5dadc22c94ee80e0e7f1a7da75be87f9c345b9
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-11 05:32:21 +0900

    [Bronze III] Title: 약수 구하기, Time: 104 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            if ((N % i) == 0) {
+                list.add(i);
+            }
+        }
+
+        int result = (list.size() >= K) ? list.get(K - 1) : 0;
+
+        System.out.println(result);
+    }
+}
