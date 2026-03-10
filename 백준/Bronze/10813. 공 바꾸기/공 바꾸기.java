e430bea842b72407ce6a31061d42e7e36e7a908a
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-10 14:45:41 +0900

    [Bronze II] Title: 공 바꾸기, Time: 112 ms, Memory: 14296 KB -BaekjoonHub

---
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // 입력
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+
+        // 변수 할당
+        int n = Integer.parseInt(input[0]);
+        int m = Integer.parseInt(input[1]);
+
+        int[] baskets = new int[n];
+
+        // 바구니 초기화
+        for (int i = 0; i < n; i++) {
+            baskets[i] = i + 1; // 인덱스 + 1
+        }
+
+        // 변수 선언
+        int i;
+        int j;
+        int temp;
+
+        // 반복문
+        for (int k = 0; k < m; k++) {
+            input = br.readLine().split(" ");
+            // 인덱스 기준으로 계산하기 위해 -1 보정
+            i = Integer.parseInt(input[0]) - 1;
+            j = Integer.parseInt(input[1]) - 1;
+
+            // 교환
+            temp = baskets[i];
+            baskets[i] = baskets[j];
+            baskets[j] = temp;
+        }
+
+        // 출력
+        StringBuilder sb = new StringBuilder();
+        for (int k = 0; k < n; k++) {
+            sb.append(baskets[k]);
+            if (k < n - 1) {
+                sb.append(' ');
+            }
+        }
+        System.out.println(sb);
+    }
+}
