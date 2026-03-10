9d180a60eb80bfee32c195729ad45d2208496309
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-10 18:11:11 +0900

    [Bronze V] Title: 문자와 문자열, Time: 108 ms, Memory: 14148 KB -BaekjoonHub

--- 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -3,21 +3,12 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        // try-with-resources for auto close
-        try (
-            // Create a BufferedReader class for faster input
-            BufferedReader br =
-                    new BufferedReader(new InputStreamReader(System.in))
-        ) {
-            // Assign variables
-            String string = br.readLine();
-            int index = Integer.parseInt(br.readLine()) - 1;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String S = br.readLine();
+        int i = Integer.parseInt(br.readLine());
 
-            // Prints charAt
-            System.out.println(string.charAt(index));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        char[] charArray = S.toCharArray();
+        System.out.println(charArray[i - 1]);
     }
 }
