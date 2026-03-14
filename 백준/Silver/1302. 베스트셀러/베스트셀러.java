ae92e2a272d2907b804c9620c3c8afb7f87e5db8
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-14 11:51:35 +0900

    [Silver IV] Title: 베스트셀러, Time: 96 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -11,26 +11,39 @@ public class Main {
 
         for (int i = 0; i < N; i++) {
             String title = br.readLine();
-            if (map.containsKey(title)) {
-                map.put(title, map.get(title) + 1);
-            } else {
-                map.put(title, 1);
-            }
+            map.put(title, map.getOrDefault(title, 0) + 1);
+
+//            if (map.containsKey(title)) {
+//                map.put(title, map.get(title) + 1);
+//            } else {
+//                map.put(title, 1);
+//            }
         }
 
         int max = 0;
-        String answer = "";
-        for (String title : map.keySet()) {
-            Integer count = map.get(title);
-            if (max < count) {
+        String answer = null;
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String title = entry.getKey();
+            int count = entry.getValue();
+
+            if (answer == null || count > max || (count == max && title.compareTo(answer) < 0)) {
                 max = count;
                 answer = title;
-            } else if (max == count) {
-                if (answer.compareTo(title) > 0) {
-                    answer = title;
-                }
             }
         }
+
+//        String answer = "";
+//        for (String title : map.keySet()) {
+//            Integer count = map.get(title);
+//            if (max < count) {
+//                max = count;
+//                answer = title;
+//            } else if (max == count) {
+//                if (answer.compareTo(title) > 0) {
+//                    answer = title;
+//                }
+//            }
+//        }
 
         System.out.println(answer);
     }
