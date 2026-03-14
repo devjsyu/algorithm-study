25c57abf15ade4724d4671783490ed93ab82af18
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-14 11:26:43 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -12,29 +12,26 @@ public class Main {
         for (int i = 0; i < N; i++) {
             String title = br.readLine();
             if (map.containsKey(title)) {
-                Integer count = map.get(title);
-                count++;
-                map.put(title, count);
+                map.put(title, map.get(title) + 1);
             } else {
                 map.put(title, 1);
             }
         }
 
-        ArrayList<Map.Entry<String, Integer>> list = new ArrayList<>(map.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<String, Integer>>() {
-            @Override
-            public int compare(Map.Entry<String, Integer> t1, Map.Entry<String, Integer> t2) {
-                if (t1.getValue() > t2.getValue()) {
-                    return -1;
-                } else if (t1.getValue() < t2.getValue()) {
-                    return 1;
-                } else {
-                    return t1.getKey().compareTo(t2.getKey());
+        int max = 0;
+        String answer = "";
+        for (String title : map.keySet()) {
+            Integer count = map.get(title);
+            if (max < count) {
+                max = count;
+                answer = title;
+            } else if (max == count) {
+                if (answer.compareTo(title) > 0) {
+                    answer = title;
                 }
             }
-        });
+        }
 
-        System.out.println(list.get(0).getKey());
+        System.out.println(answer);
     }
 }
