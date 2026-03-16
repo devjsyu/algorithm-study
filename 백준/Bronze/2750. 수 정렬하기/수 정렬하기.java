e6dae72c2487931943d6ea1835e62ea3ff7af3a8
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-16 11:05:58 +0900

    [Bronze II] Title: 수 정렬하기, Time: 128 ms, Memory: 14492 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,11 +10,40 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        Arrays.sort(arr);
+
+        quickSort(arr, 0, N - 1);
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             sb.append(arr[i]).append('\n');
         }
         System.out.println(sb);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pivot = partition(arr, low, high);
+            quickSort(arr, low, pivot - 1);
+            quickSort(arr, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivotElement = arr[high];
+        int checked = low - 1;
+        for (int i = low; i <= high - 1; i++) {
+            if (arr[i] < pivotElement) {
+                checked++;
+
+                int temp = arr[checked];
+                arr[checked] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        int temp = arr[high];
+        arr[high] = arr[checked + 1];
+        arr[checked + 1] = temp;
+
+        return checked + 1;
     }
 }
