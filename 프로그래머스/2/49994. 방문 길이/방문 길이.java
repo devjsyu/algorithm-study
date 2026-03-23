0def522be9b330edac0cd9e6a7e91405664e56b6
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-23 13:34:11 +0900

    [level 2] Title: 방문 길이, Time: 19.58 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -3,47 +3,40 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(String dirs) {
-        // store x, y coordinate in an array
-        
-        // initialize x, y coordinate
-        int x = 5;
-        int y = 5;
-        
-        // initialize the commands with HashMap
-        HashMap<Character, int[]> map = new HashMap<>();
-        map.put('U', new int[]{0, 1});
-        map.put('D', new int[]{0, -1});
-        map.put('R', new int[]{1, 0});
-        map.put('L', new int[]{-1, 0});
+        int[] coordinate = new int[]{0, 0};
+        HashMap<Character, Integer[]> commands = new HashMap<>();
+        commands.put('U', new Integer[]{0, 1});
+        commands.put('D', new Integer[]{0, -1});
+        commands.put('R', new Integer[]{1, 0});
+        commands.put('L', new Integer[]{-1, 0});
 
-        // store every path with HashSet
-        HashSet<String> set = new HashSet<>();
+        HashSet<String> paths = new HashSet<>();
 
-        // for-loop
         for (int i = 0; i < dirs.length(); i++) {
-            int[] offset = map.get(dirs.charAt(i)); // offset for the given command
-            int xMoved = x + offset[0];
-            int yMoved = y + offset[1];
-            
-            // continue the loop if it's not valid
-            if (!isValid(xMoved, yMoved)) {
-                continue;
+            char c = dirs.charAt(i);
+            Integer[] move = commands.get(c);
+            int x = coordinate[0];
+            int y = coordinate[1];
+            int xMove = move[0];
+            int yMove = move[1];
+            int xMoved = x + xMove;
+            int yMoved = y + yMove;
+            if (!isOutOfRange(xMoved, yMoved)) {
+                coordinate[0] = xMoved;
+                coordinate[1] = yMoved;
+                
+                paths.add(x + " " + xMoved + " " + y + " " + yMoved);
+                paths.add(xMoved + " " + x + " " + yMoved + " " + y);
             }
-
-            // double the path and divide it with 2 at the end
-            set.add(x + " " + y + " " + xMoved + " " + yMoved);
-            set.add(xMoved + " " + yMoved + " " + x + " " + y);
-            
-            // update x, y coordinate for the next loop
-            x = xMoved;
-            y = yMoved;
         }
-        
-        return set.size() / 2;
+
+        int answer = paths.size() / 2;
+        return answer;
     }
 
-    // isValid
-    private boolean isValid(int xMoved, int yMoved) {
-        return 0 <= xMoved && xMoved < 11 && 0 <= yMoved && yMoved < 11;
+    private static boolean isOutOfRange(int xMoved, int yMoved) {
+        int xMovedAbs = Math.abs(xMoved);
+        int yMovedAbs = Math.abs(yMoved);
+        return xMovedAbs > 5 || yMovedAbs > 5;
     }
 }
