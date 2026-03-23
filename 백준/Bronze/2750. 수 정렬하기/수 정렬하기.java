7b703f5eae309f2a3097459b8522e7191726677e
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-23 13:02:05 +0900

    [Bronze II] Title: 수 정렬하기, Time: 120 ms, Memory: 14460 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -5,17 +5,17 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
+        int total = Integer.parseInt(br.readLine());
+        int[] arr = new int[total];
+        for (int i = 0; i < total; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
-        quickSort(arr, 0, N - 1);
+        quickSort(arr, 0, total - 1);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i]).append('\n');
+        for (int i : arr) {
+            sb.append(i).append('\n');
         }
         System.out.println(sb);
     }
@@ -23,27 +23,27 @@ public class Main {
     private static void quickSort(int[] arr, int low, int high) {
         if (low < high) {
             int pivot = partition(arr, low, high);
+
             quickSort(arr, low, pivot - 1);
             quickSort(arr, pivot + 1, high);
         }
     }
 
     private static int partition(int[] arr, int low, int high) {
-        int pivotElement = arr[high];
-        int checked = low - 1;
+        int index = low - 1;
+        int pivotValue = arr[high];
         for (int i = low; i <= high - 1; i++) {
-            if (arr[i] < pivotElement) {
-                checked++;
-
-                int temp = arr[checked];
-                arr[checked] = arr[i];
+            if (arr[i] < pivotValue) {
+                index++;
+                int temp = arr[index];
+                arr[index] = arr[i];
                 arr[i] = temp;
             }
         }
-        int temp = arr[high];
-        arr[high] = arr[checked + 1];
-        arr[checked + 1] = temp;
+        int temp = arr[index + 1];
+        arr[index + 1] = pivotValue;
+        arr[high] = temp;
 
-        return checked + 1;
+        return index + 1;
     }
 }
