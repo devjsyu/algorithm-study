aca07afef9d4644d4fc1854f36a4c6747e9e9e03
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-24 10:31:02 +0900

    [Bronze II] Title: 진법 변환, Time: 108 ms, Memory: 14384 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.java
+++ 백준/Bronze/2745. 진법 변환/진법 변환.java
@@ -1,34 +1,20 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        char[] N = st.nextToken().toCharArray();
-        int[] digits = new int[N.length];
+        String[] input = br.readLine().split(" ");
+        String N = input[0];
+        int exp = Integer.parseInt(input[1]);
 
-        // 주어진 N에서 각 자리 숫자를 10진법으로 치환한 배열 생성
-        for (int i = N.length - 1; i >= 0 ; i--) {
-            if (Character.isAlphabetic(N[i])) {
-                digits[i] = ((int) N[i]) - 55; // 'A' ~ 'Z'의 ASCII value에서 55 차감하여 문제 조건과 일치시킴
-            } else {
-                digits[i] = ((int) N[i]) - 48; // '0' ~ '9'의 ASCII value에서 48 차감하여 문제 조건과 일치시킴
-            }
+        int length = N.length();
+        int answer = 0;
+        for (int i = 0; i < length; i++) {
+            answer += (int) (Math.pow(exp, i) * Character.getNumericValue(N.charAt(length - 1 - i)));
         }
 
-        int B = Integer.parseInt(st.nextToken());
-
-        int decimal = 0;
-        int k = 0;
-        for (int i = N.length - 1; i >= 0 ; i--) {
-            // 10진법으로 치환된 각 자리 숫자와 각 자리의 B진법 제곱을 곱한 결과를 십진법 숫자 변수에 누적 합산
-            decimal += (int) Math.pow(B, k) * digits[i];
-            k++;
-        }
-
-        System.out.println(decimal);
+        System.out.println(answer);
     }
 }
