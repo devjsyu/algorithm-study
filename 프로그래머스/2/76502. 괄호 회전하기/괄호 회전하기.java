0c676ed5cc56f4ece37fe6abb5e154abccbf9046
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-25 13:20:53 +0900

    [level 2] Title: 괄호 회전하기, Time: 47.96 ms, Memory: 88.1 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,48 +1,39 @@
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 class Solution {
     public int solution(String s) {
-        int length = s.length();
-        ArrayDeque<Character> queue = new ArrayDeque<>();
-        for (char c : s.toCharArray()) {
-            queue.add(c);
-        }
+        // 괄호 종류별 열고 닫는 문자 짝지어 저장하기
         HashMap<Character, Character> map = new HashMap<>();
         map.put('(', ')');
         map.put('[', ']');
         map.put('{', '}');
 
+        int length = s.length();
+
+        // 문자열 회전을 위해 동일한 문자열 연달아 이어붙이기
+        s += s;
+
         int count = 0;
-        for (int i = 0; i < length; i++) {
-            if (i != 0) {
-                queue.add(queue.remove());
+        
+        // 문자열 회전을 위해 이중 반복문으로 순회하기 
+        A:for (int i = 0; i < length; i++) {
+            ArrayDeque<Character> stack = new ArrayDeque<>();
+            for (int j = i; j < i + length; j++) {
+                char c = s.charAt(j);
+                if (map.containsKey(c)) {
+                    stack.push(c);
+                } else if (stack.isEmpty() || map.get(stack.peek()) != c) {
+                    continue A;
+                } else {
+                    stack.pop();
+                }
             }
-            ArrayList<Character> list = new ArrayList<>(queue);
-            if (isValid(list, map)) {
+            if (stack.isEmpty()) {
                 count++;
             }
         }
 
         return count;
-    }
-
-    private boolean isValid(List<Character> list, HashMap<Character, Character> map) {
-        ArrayDeque<Character> stack = new ArrayDeque<>();
-        for (int i = 0; i < list.size(); i++) {
-            Character removed = list.get(i);
-            if (map.containsKey(removed)) {
-                stack.push(removed);
-            } else if (stack.isEmpty()) {
-                return false;
-            } else if (removed == map.get(stack.peek())) {
-                stack.pop();
-            } else {
-                return false;
-            }
-        }
-        return stack.isEmpty();
     }
 }
