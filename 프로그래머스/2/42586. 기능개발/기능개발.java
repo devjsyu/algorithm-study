5a3de7a2d4bf803ee39f55fba53ce13975ccfe17
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-03-31 12:26:44 +0900

    [level 2] Title: 기능개발, Time: 2.93 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,37 +1,26 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Queue;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new ArrayDeque<>();
-
+        ArrayDeque<Integer> queue = new ArrayDeque<>();
         for (int i = 0; i < progresses.length; i++) {
-            int job = 100 - progresses[i];
-            int days = job % speeds[i] == 0 ? job / speeds[i] : job / speeds[i] + 1;
-            queue.add(days);
+            queue.add((100 - progresses[i] + speeds[i] - 1) / speeds[i]);
         }
-
         ArrayList<Integer> list = new ArrayList<>();
+        Integer bottleneck = queue.poll();
         int count = 1;
-
-        if (queue.isEmpty()) {
-            return new int[]{0};
-        }
-        int limit = queue.poll();
-
         while (!queue.isEmpty()) {
-            int poll = queue.poll();
-            if (limit >= poll) {
-                count++;
-            } else {
+            Integer work = queue.poll();
+            if (bottleneck != null && bottleneck < work) {
+                bottleneck = work;
                 list.add(count);
                 count = 1;
-                limit = poll;
+            } else {
+                count++;
             }
         }
         list.add(count);
-        
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
