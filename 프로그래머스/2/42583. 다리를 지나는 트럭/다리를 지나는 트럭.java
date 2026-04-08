bc99fd728065be94d8cf7e409f53fa76afe84a09
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-04-08 10:49:10 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 17.04 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,29 +2,35 @@ import java.util.ArrayDeque;
 import java.util.Queue;
 
 class Solution {
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
+    public static void main(String[] args) {
+        int bridge_length = 2;
+        int weight = 10;
+        int[] truck_weights = {7,4,5,6};
+        int solution = solution(bridge_length, weight, truck_weights);
+        System.out.println(solution);
+    }
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
         Queue<Integer> bridge = new ArrayDeque<>();
         int totalWeight = 0;
-        int timeSpent = 0;
+        int seconds = 0;
+
         for (int i = 0; i < truck_weights.length; i++) {
-            while (true) {
-                if (!bridge.isEmpty() && bridge.size() == bridge_length) {
-                    int currentWeight = bridge.poll();
-                    totalWeight -= currentWeight;
+            while (totalWeight + truck_weights[i] > weight) {
+                if (bridge.size() >= bridge_length) {
+                    int truckGone = bridge.poll();
+                    totalWeight -= truckGone;
                 } else {
-                    if (weight - totalWeight >= truck_weights[i]) {
-                        int currentWeight = truck_weights[i];
-                        bridge.add(currentWeight);
-                        totalWeight += currentWeight;
-                        timeSpent++;
-                        break;
-                    } else {
-                        bridge.add(0);
-                        timeSpent++;
-                    }
+                    bridge.add(0);
+                    seconds++;
                 }
             }
+            bridge.add(truck_weights[i]);
+            totalWeight += truck_weights[i];
+            seconds++;
         }
-        return timeSpent + bridge_length;
+
+        seconds += bridge_length;
+        
+        return seconds;
     }
 }
