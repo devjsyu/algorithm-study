0f1fda179a17bd2e28a76acbf88b78153eb14242
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-04-17 10:14:35 +0900

    [level 2] Title: 예상 대진표, Time: 0.03 ms, Memory: 89.9 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,29 +2,16 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        return countRounds(1, a, b);
-    }
-
-    private int countRounds(int count, int a, int b) {
-        if (a > b) {
-            int temp = a;
-            a = b;
-            b = temp;
+        int answer = 0;
+        while (true) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
+            answer++;
+            if (a == b) {
+                break;
+            }
         }
 
-        if (a + 1 == b && b % 2 == 0) {
-            return count;
-        } else {
-            return countRounds(++count, nextRound(a), nextRound(b));
-        }
-    }
-
-    private int nextRound(int x) {
-        if (x % 2 == 0) {
-            x /= 2;
-        } else {
-            x = (x + 1) / 2;
-        }
-        return x;
+        return answer;
     }
 }
