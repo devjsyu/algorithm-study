3aab4fd6dde2d47394efaf6f7aa55fb6bea4cdbb
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-04-23 11:01:03 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14276 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -6,20 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int sqrt = (int) Math.sqrt(N);
         StringBuilder sb = new StringBuilder();
-        for (int i = 2; i <=sqrt ; i++) {
+        if (N == 1) {
+            return;
+        }
+        int sqrt = (int) Math.sqrt(N);
+        for (int i = 2; i <= sqrt; i++) {
             while (N % i == 0) {
-                N = N / i;
+                N /= i;
                 sb.append(i).append('\n');
-            }
-            if (N == 1) {
-                break;
             }
         }
         if (N != 1) {
             sb.append(N).append('\n');
         }
-        System.out.print(sb);
+        System.out.println(sb);
     }
 }
