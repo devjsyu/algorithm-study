b94f26506c54cfeac7279ba51695351747810a9d
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-05-08 15:57:35 +0900

    [level 2] Title: 방문 길이, Time: 27.15 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -3,40 +3,43 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(String dirs) {
-        int[] coordinate = new int[]{0, 0};
-        HashMap<Character, Integer[]> commands = new HashMap<>();
-        commands.put('U', new Integer[]{0, 1});
-        commands.put('D', new Integer[]{0, -1});
-        commands.put('R', new Integer[]{1, 0});
-        commands.put('L', new Integer[]{-1, 0});
+        // 초기 좌표
+        int[] coordinate = {0, 0};
 
-        HashSet<String> paths = new HashSet<>();
+        // 루트 저장할 자료구조 초기화
+        // 중복 제거를 위해 HashSet 사용
+        HashSet<String> routes = new HashSet<>();
+        
+        // 명령어
+        HashMap<Character, int[]> command = new HashMap<>();
+        command.put('U', new int[]{0, 1});
+        command.put('D', new int[]{0, -1});
+        command.put('R', new int[]{1, 0});
+        command.put('L', new int[]{-1, 0});
+        
+        // 순회
+        for (char c : dirs.toCharArray()) {
+            int[] coordinateMove = command.get(c);
+            int tempX = coordinate[0] + coordinateMove[0];
+            int tempY = coordinate[1] + coordinateMove[1];
 
-        for (int i = 0; i < dirs.length(); i++) {
-            char c = dirs.charAt(i);
-            Integer[] move = commands.get(c);
-            int x = coordinate[0];
-            int y = coordinate[1];
-            int xMove = move[0];
-            int yMove = move[1];
-            int xMoved = x + xMove;
-            int yMoved = y + yMove;
-            if (!isOutOfRange(xMoved, yMoved)) {
-                coordinate[0] = xMoved;
-                coordinate[1] = yMoved;
-                
-                paths.add(x + " " + xMoved + " " + y + " " + yMoved);
-                paths.add(xMoved + " " + x + " " + yMoved + " " + y);
+            // 지정 범위 벗어나면 필터링
+            if (tempX < -5 || tempY < -5 || tempX > 5 || tempY > 5) {
+                continue;
             }
+            
+            // 방향 구분 없이 일괄 저장
+            routes.add(coordinate[0] + ", " + coordinate[1] + " - " + tempX + ", " + tempY);
+            routes.add(tempX + ", " + tempY + " - " +  coordinate[0] + ", " + coordinate[1]);
+
+            // 좌표 갱신
+            coordinate[0] = tempX;
+            coordinate[1] = tempY;
         }
+        
+        // 최종 집계
+        int answer = routes.size() / 2;
 
-        int answer = paths.size() / 2;
         return answer;
-    }
-
-    private static boolean isOutOfRange(int xMoved, int yMoved) {
-        int xMovedAbs = Math.abs(xMoved);
-        int yMovedAbs = Math.abs(yMoved);
-        return xMovedAbs > 5 || yMovedAbs > 5;
     }
 }
