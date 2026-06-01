0e9c08b0de46b030eba2625cc4190b0c934c72bd
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-06-01 16:16:03 +0900

    [level 2] Title: 기능개발, Time: 2.18 ms, Memory: 66.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,34 @@
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
+        Deque<Integer> queue = new ArrayDeque<>();
+
         for (int i = 0; i < progresses.length; i++) {
-            queue.add((100 - progresses[i] + speeds[i] - 1) / speeds[i]);
+            int remainingDays = (100 - progresses[i] + speeds[i] - 1) / speeds[i]; // 나눗셈의 몫을 무조건 올림
+            queue.offer(remainingDays);
         }
-        ArrayList<Integer> list = new ArrayList<>();
-        Integer bottleneck = queue.poll();
-        int count = 1;
+
+        List<Integer> list = new ArrayList<>();
+        int count = 0;
+        int maxRemainingDays = (100 - progresses[0] + speeds[0] - 1) / speeds[0]; // 첫 번째 요소
+
         while (!queue.isEmpty()) {
-            Integer work = queue.poll();
-            if (bottleneck != null && bottleneck < work) {
-                bottleneck = work;
+            int peek = queue.peek();
+            if (maxRemainingDays < peek) {
+                maxRemainingDays = peek;
                 list.add(count);
-                count = 1;
+                count = 0;
             } else {
+                queue.poll();
                 count++;
             }
         }
-        list.add(count);
+        list.add(count); // 마지막 요소 추가
+
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
