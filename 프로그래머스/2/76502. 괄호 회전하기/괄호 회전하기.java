886e08cb0a3a8fb9f86ee2c86bb87dabc338fe33
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-06-01 11:11:42 +0900

    [level 2] Title: 괄호 회전하기, Time: 16.28 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,37 +1,49 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.HashMap;
-import java.util.Stack;
+import java.util.Map;
 
 class Solution {
-    public int solution(String s) {
-        String extended = s + s;
-        char[] charArray = extended.toCharArray();
-        
-        int answer = 0;
+    // 자주 쓰이는 변수는 멤버 변수로 선언
+    private final Map<Character, Character> map = new HashMap<>();
+    private char[] charArray;
+    private int length;
 
-        Stack<Character> stack = new Stack<>();
-        HashMap<Character, Character> map = new HashMap<>();
+    public int solution(String s) {
+        // 멤버 변수 초기화
         map.put(')', '(');
         map.put(']', '[');
         map.put('}', '{');
 
-        outerLoop:
-        for (int i = 0; i < s.length(); i++) {
-            stack.clear();
+        String extended = s + s; // 주어진 문자열을 2배로 늘린 상태에서 일부 구간 순회하여 회전 구현
+        charArray = extended.toCharArray();
+        length = s.length();
 
-            for (int j = i; j < i + s.length(); j++) {
-                char c = charArray[j];
-                if (!map.containsKey(c)) {
-                    stack.push(c);
-                } else if (!stack.isEmpty() && stack.peek() == map.get(c)) {
-                    stack.pop();
-                } else {
-                    continue outerLoop;
-                }
-            }
-            if (stack.isEmpty()) {
+        int answer = 0;
+
+        for (int i = 0; i < length; i++) {
+            // 유효한 문자열인지 검증
+            if (isValid(i)) {
                 answer++;
             }
         }
+
         return answer;
+    }
+
+    private boolean isValid(int start) {
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (int i = start; i < start + length; i++) {
+            char c = charArray[i];
+            if (!map.containsKey(c)) {
+                stack.push(c);
+            } else if (!stack.isEmpty() && stack.peek() == map.get(c)) {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
     }
 }
