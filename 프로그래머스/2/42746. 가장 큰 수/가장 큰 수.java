a74c69b0cd98b849fca6353f5b6855803d0decfe
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-06-02 10:41:33 +0900

    [level 2] Title: 가장 큰 수, Time: 103.79 ms, Memory: 116 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,37 +1,28 @@
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 class Solution {
     public String solution(int[] numbers) {
-        // Comparator
-        Comparator<Integer> comparator = new Comparator<>() {
+        StringBuilder sb1 = new StringBuilder();
+        StringBuilder sb2 = new StringBuilder();
+        Arrays.stream(numbers).mapToObj(String::valueOf).sorted(new Comparator<String>() {
             @Override
-            public int compare(Integer o1, Integer o2) {
-                int num1 = o1;
-                int o1Digit = num1 == 0 ? 1 : 0;
-                while (num1 > 0) {
-                    num1 /= 10;
-                    o1Digit++;
-                }
+            public int compare(String s1, String s2) {
+                sb2.append(s1).append(s2);
+                String string1 = sb2.toString();
+                sb2.setLength(0);
 
-                int num2 = o2;
-                int o2Digit = num2 == 0 ? 1 : 0;
-                while (num2 > 0) {
-                    num2 /= 10;
-                    o2Digit++;
-                }
+                sb2.append(s2).append(s1);
+                String string2 = sb2.toString();
+                sb2.setLength(0);
 
-                // comparison between ab ba
-                return (int) ((o2 * Math.pow(10, o1Digit) + o1) - (o1 * Math.pow(10, o2Digit) + o2));
+                return string1.compareTo(string2);
             }
-        };
-        // Stream
-        Stream<Integer> boxed = Arrays.stream(numbers).boxed();
-        String answer = boxed.sorted(comparator).map(String::valueOf).collect(Collectors.joining());
-
-        if (answer.charAt(0) == '0') answer = "0";
+        }.reversed()).forEach(sb1::append);
+        String answer = sb1.toString();
+        if (answer.startsWith("0")) {
+            return "0";
+        }
         return answer;
     }
 }
