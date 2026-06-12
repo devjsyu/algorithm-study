6d13f7f801542a22bff30cad17949382630d36e9
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-06-12 15:42:21 +0900

    Time: 2 ms (99.2%), Space: 44.1 MB (91.25%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,32 +1,20 @@
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.List;
-
 class Solution {
     public boolean isPalindrome(String s) {
-        List<Character> list = new ArrayList<>();
+        int left = 0;
+        int right = s.length() - 1;
 
-        // 문자 변경
-        char[] charArray = s.toCharArray();
-        for (char c : charArray) {
-            if (!Character.isLetterOrDigit(c)) {
-                continue;
+        while (left < right) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(left))) {
+                left++;
             }
-            c = Character.toLowerCase(c);
-            list.add(c);
-        }
-
-        // Two Pointers를 위한 변수 및 배열 초기화
-        int p1 = 0;
-        int p2 = list.size() - 1;
-        Character[] array = list.toArray(Character[]::new);
-
-        // 팰린드롬 부합 여부 확인
-        for (int i = 0; i < array.length; i++) {
-            if (array[p1++] != array[p2--]) {
+            while (left < right && !Character.isLetterOrDigit(s.charAt(right))) {
+                right--;
+            }
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
                 return false;
             }
+            left++;
+            right--;
         }
 
         return true;
