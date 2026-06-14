30ce63f67a1f3229df2896de97bcf2112d0afec3
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-06-14 16:11:56 +0900

    [level 2] Title: 기능개발, Time: 2.48 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,34 +1,36 @@
-import java.util.ArrayDeque;
 import java.util.ArrayList;
-import java.util.Deque;
 import java.util.List;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Deque<Integer> queue = new ArrayDeque<>();
+        // Two Pointers : 현재 그룹의 가장 앞선 요소와 현재 검사하는 요소
+        int left = 0;
+        int right = left + 1;
+
+        int[] daysLeftForCompletion = new int[progresses.length];
 
         for (int i = 0; i < progresses.length; i++) {
-            int remainingDays = (100 - progresses[i] + speeds[i] - 1) / speeds[i]; // 나눗셈의 몫을 무조건 올림
-            queue.offer(remainingDays);
+            // 완성 소요 일수 계산
+            daysLeftForCompletion[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
         }
 
         List<Integer> list = new ArrayList<>();
-        int count = 0;
-        int maxRemainingDays = (100 - progresses[0] + speeds[0] - 1) / speeds[0]; // 첫 번째 요소
+        int count = 1;
 
-        while (!queue.isEmpty()) {
-            int peek = queue.peek();
-            if (maxRemainingDays < peek) {
-                maxRemainingDays = peek;
-                list.add(count);
-                count = 0;
-            } else {
-                queue.poll();
+        while (right < progresses.length) {
+            // 가장 앞선 요소의 소요 일수보다 큰 경우가 나올 때까지 배포 개수 누적
+            if (daysLeftForCompletion[left] >= daysLeftForCompletion[right]) {
                 count++;
+                right++;
+            } else {
+                left = right;
+                right++;
+                list.add(count);
+                count = 1;
             }
         }
-        list.add(count); // 마지막 요소 추가
 
+        list.add(count); // 마지막 그룹까지 집계
         return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
