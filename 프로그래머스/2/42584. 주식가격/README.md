0ae845156dd15a8c150ec73a55732d6534c3fb22
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-18 15:03:54 +0900

    [level 2] Title: 주식가격, Time: 13.72 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
 
 ### 성능 요약
 
-메모리: 79.2 MB, 시간: 39.58 ms
+메모리: 72.8 MB, 시간: 13.72 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 05일 13:20:56
+2026년 07월 18일 15:03:50
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,19 +1,29 @@
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 class Solution {
     public int[] solution(int[] prices) {
+        Deque<Integer> stack = new ArrayDeque<>();
         int[] answer = new int[prices.length];
-        Stack<Integer> stack = new Stack<>();
-        stack.push(0);
-        for (int i = 1; i < prices.length; i++) {
+
+        // 가격 배열 순회
+        for (int i = 0; i < prices.length; i++) {
+            // 가격 하락했을 경우
             while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
-                int index = stack.pop();
-                answer[index] = i - index;
+                // 가격 하락한 시점 가격 인덱스
+                int priceDecreased = stack.pop();
+                // 정답 배열의 해당 인덱스에 가격 유지 기간 계산값 할당
+                answer[priceDecreased] = i - priceDecreased;
             }
+
+            // 가격 하락 이외 경우
             stack.push(i);
         }
+
+        // Stack에 남아있는 인덱스는 단 한번도 가격 하락하지 않은 경우
+        // 순회하면서 가격 유지 기간 계산
         for (Integer i : stack) {
-            answer[i] = prices.length - i - 1;
+            answer[i] = prices.length - 1 - i;
         }
 
         return answer;
