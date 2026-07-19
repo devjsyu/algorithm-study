d06b0efc00f68da07d38861cbf1efe708e1968da
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-19 13:07:25 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 55.34 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -3,26 +3,27 @@ import java.util.Map;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        HashMap<String, Integer> map = new HashMap<>();
-        String answer = "";
-        
+        // 이름별 집계를 위한 자료구조 초기화
+        HashMap<String, Integer> nameByCount = new HashMap<>();
+
+        // participant 배열 순회하면서 이름별 집계
         for (String name : participant) {
-            map.put(name, map.getOrDefault(name, 0) + 1);
-        }
-        for (String name : completion) {
-            if (map.containsKey(name)) {
-                map.put(name, map.get(name) - 1);
-            } else {
-                return name;
-            }
+            // 빈도수 집계
+            nameByCount.put(name, nameByCount.getOrDefault(name, 0) + 1); // getOrDefault 메서드 사용: null 체크 생략 및 NullPointerException 예방
         }
 
-        for (Map.Entry<String, Integer> entry : map.entrySet()) {
-            if (entry.getValue().equals(1)) {
-                answer = entry.getKey();
-                return answer;
+        // completion 배열 순회하면서 이름별 집계에서 해당 이름 차감
+        for (String name : completion) {
+            // 차감
+            nameByCount.computeIfPresent(name, (k, v) -> v - 1);
+        }
+        
+        // 유일하게 남아있는 이름 반환
+        for (Map.Entry<String, Integer> entry : nameByCount.entrySet()) {
+            if (entry.getValue() == 1) {
+                return entry.getKey();
             }
         }
-        return answer;
+        return null;
     }
 }
