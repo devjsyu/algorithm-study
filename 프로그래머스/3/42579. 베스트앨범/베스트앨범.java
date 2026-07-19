7f72b90cc86ca39cf0773a21caf391309b9bc9ae
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-19 20:46:41 +0900

    [level 3] Title: 베스트앨범, Time: 20.56 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,34 +2,61 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        Map<String, Integer> genresToPlayCount = new HashMap<>();
-        Map<String, List<int[]>> genresToPlayCountById = new HashMap<>();
+    // 준비
+        // 장르별 집계를 위한 자료구조 초기화
+        Map<String, Integer> genreToPlayCount = new HashMap<>();
+
+        Map<Integer, GenreToPlayCount> idToGenreAndPlayCount = new HashMap<>();
 
         for (int i = 0; i < genres.length; i++) {
-            genresToPlayCount.put(genres[i], genresToPlayCount.getOrDefault(genres[i], 0) + plays[i]);
-            if (!genresToPlayCountById.containsKey(genres[i])) {
-                genresToPlayCountById.put(genres[i], new ArrayList<>());
-            }
-            genresToPlayCountById.get(genres[i]).add(new int[]{i, plays[i]});
+            idToGenreAndPlayCount.put(i, new GenreToPlayCount(genres[i], plays[i]));
+        }
+        Set<Map.Entry<Integer, GenreToPlayCount>> idToGenreAndPlayCountEntrySet = idToGenreAndPlayCount.entrySet();
+
+        // 정답 담을 리스트 초기화
+        List<Integer> answer = new ArrayList<>();
+
+
+    // 1차 정렬 : 많이 재생된 장르 순
+        // 장르별 집계
+        for (int i = 0; i < genres.length; i++) {
+            genreToPlayCount.put(genres[i], genreToPlayCount.getOrDefault(genres[i], 0) + plays[i]);
         }
 
-        List<Map.Entry<String, Integer>> entries = new ArrayList<>(genresToPlayCount.entrySet());
-        entries.sort((a, b) -> b.getValue().compareTo(a.getValue()));
+        // 많이 재생된 장르 순 정렬
+        List<String> genreByPopularityOrdered = genreToPlayCount.entrySet().stream()
+                .sorted((e1, e2) -> e2.getValue() - e1.getValue())
+                .map(Map.Entry::getKey)
+                .toList();
 
-        ArrayList<Integer> list = new ArrayList<>();
-        for (Map.Entry<String, Integer> entry : entries) {
-            genresToPlayCountById.get(entry.getKey()).stream().sorted(new Comparator<int[]>() {
-                @Override
-                public int compare(int[] o1, int[] o2) {
-                    if (o1[1] == o2[1]) {
-                        return o1[0] - o2[0]; // 고유번호 오름차순 정렬
-                    } else {
-                        return o2[1] - o1[1]; // 재생횟수 내림차순 정렬
-                    }
-                }
-            }).limit(2).forEach(o -> list.add(o[0]));
+    // 2차 정렬 : 재생횟수, 고유번호 순 정렬
+        // 장르 순회
+        for (int i = 0; i < genreByPopularityOrdered.size(); i++) {
+            int index = i;
+            idToGenreAndPlayCountEntrySet.stream()
+                    // 장르별 처리
+                    .filter(o -> o.getValue().genre.equals(genreByPopularityOrdered.get(index)))
+                    // 정렬 조건 만족하는 custom comparator 선언
+                    .sorted((o1, o2) -> {
+                        // 재생횟수가 같은 경우 고유번호 오름차순 정렬
+                        if (o1.getValue().playerCount.equals(o2.getValue().playerCount)) {
+                            return o1.getKey().compareTo(o2.getKey());
+                        } else {
+                            // 재생횟수 내림차순 정렬
+                            return o2.getValue().playerCount.compareTo(o1.getValue().playerCount);
+                        }
+                    })
+                    // 필요한 정보만 필터링 가공
+                    .map(Map.Entry::getKey)
+                    // Top 2
+                    .limit(2)
+                    // 정답 리스트에 추가
+                    .forEach(answer::add);
         }
 
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        // 정답 반환
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
+
+    public record GenreToPlayCount(String genre, Integer playerCount) {}
 }
