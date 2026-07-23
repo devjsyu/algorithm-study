7fb9a4c9e8553397c92d8b443047bcadc0e96dd7
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-23 10:50:02 +0900

    [level 2] Title: 테이블 해시 함수, Time: 7.49 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,37 +1,23 @@
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
-        int[][] sorted = Arrays.stream(data)
-                .sorted((o1, o2) -> {
-                    int colVariable1 = o1[col - 1];
-                    int colVariable2 = o2[col - 1];
-                    if (colVariable1 == colVariable2) {
-                        int pk1 = o1[0];
-                        int pk2 = o2[0];
-                        return pk2 - pk1; // PK 내림차순 정렬
-                    } else {
-                        return colVariable1 - colVariable2; // 지정된 col 값 오름차순 정렬
+        Arrays.sort(data, (o1, o2) -> {
+                    if (o1[col - 1] == o2[col - 1]) {
+                        return Integer.compare(o2[0], o1[0]); // PK 내림차순 정렬
                     }
-                }).toArray(int[][]::new);
+                    return Integer.compare(o1[col - 1], o2[col - 1]); // 지정된 col 값 오름차순 정렬
+                });
 
-        List<Integer> list = new ArrayList<>();
-
+        int answer = 0;
         for (int i = row_begin; i <= row_end; i++) {
             int temp = 0;
-            for (int value : sorted[i - 1]) {
+            for (int value : data[i - 1]) {
                 temp += value % i;
             }
-            list.add(temp);
+            answer ^= temp;
         }
-        
-        int answer = list.get(0);
-        for (int i = 1; i < list.size(); i++) {
-            answer = answer ^ list.get(i);
-        }
-        
+
         return answer;
     }
 }
