7fdff4606145b75f6b66272d2f0788ca91fcd781
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-24 11:07:20 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.24 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 같은 숫자는 싫어 - 12906 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12906) 
 
 ### 성능 요약
 
-메모리: 118 MB, 시간: 55.39 ms
+메모리: 118 MB, 시간: 31.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 03월 29일 15:49:48
+2026년 07월 24일 11:07:15
 
 ### 문제 설명
 
--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,23 +1,32 @@
 import java.util.*;
 
 public class Solution {
-    public int[] solution(int [] arr) {
-        Stack<Integer> stack = new Stack<>();
+    public int[] solution(int[] arr) {
+        // 직전 요소와 비교를 위해 Stack 사용
+            // 자료구조 초기화
+        Deque<Integer> stack = new ArrayDeque<>();
+        List<Integer> answer = new ArrayList<>();
+        
+        // 주어진 배열 순회하면서 아래 규칙에 따라 Stack 초기화
         for (int i = 0; i < arr.length; i++) {
-            if (!stack.isEmpty() && stack.peek().equals(arr[i])) {
-                continue;
+            // Stack이 비어있다면?
+            if (stack.isEmpty()) {
+                // Stack에 추가
+                stack.add(arr[i]);
             } else {
-                stack.push(arr[i]);
+            // 추가하려는 요소와 직전 요소 비교
+                // 같다면?
+                if (arr[i] == stack.peek()) {
+                    continue;
+                } else {
+                // 다르다면?
+                    answer.add(stack.pop());
+                    stack.add(arr[i]);
+                }
             }
         }
-
-        int[] answer = new int[stack.size()];
-        int i = stack.size() - 1;
-        while (!stack.isEmpty()) {
-            answer[i] = stack.pop();
-            i--;
-        }
-
-        return answer;
+        answer.add(stack.pop());
+        
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
