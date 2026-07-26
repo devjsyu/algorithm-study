e37f43ceec5b0e9f131fd9c13aee6a69d03f3d49
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-27 00:05:53 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/README.md
+++ 프로그래머스/2/42586. 기능개발/README.md
@@ -1,10 +1,10 @@
 # [level 2] 기능개발 - 42586 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42586) 
 
 ### 성능 요약
 
-메모리: 73 MB, 시간: 2.48 ms
+메모리: 83.1 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 14일 16:11:48
+2026년 07월 27일 00:05:46
 
 ### 문제 설명
 
--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,36 +1,30 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        // Two Pointers : 현재 그룹의 가장 앞선 요소와 현재 검사하는 요소
-        int left = 0;
-        int right = left + 1;
-
-        int[] daysLeftForCompletion = new int[progresses.length];
-
-        for (int i = 0; i < progresses.length; i++) {
-            // 완성 소요 일수 계산
-            daysLeftForCompletion[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
+        int[] remainingDays = new int[progresses.length];
+        for (int i = 0; i <  progresses.length; i++) {
+            remainingDays[i] = (100 - progresses[i] + speeds[i] - 1) / speeds[i];
         }
-
-        List<Integer> list = new ArrayList<>();
-        int count = 1;
-
-        while (right < progresses.length) {
-            // 가장 앞선 요소의 소요 일수보다 큰 경우가 나올 때까지 배포 개수 누적
-            if (daysLeftForCompletion[left] >= daysLeftForCompletion[right]) {
-                count++;
-                right++;
+        
+        List<Integer> temp = new ArrayList<>();
+        int front = remainingDays[0];
+        int sum = 1;
+        for (int i = 1; i <  progresses.length; i++) {
+            if (front >= remainingDays[i]) {
+                sum++;
             } else {
-                left = right;
-                right++;
-                list.add(count);
-                count = 1;
+                front = remainingDays[i];
+                temp.add(sum);
+                sum = 1;
             }
         }
-
-        list.add(count); // 마지막 그룹까지 집계
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        temp.add(sum);
+        
+        int[] answer = new int[temp.size()];
+        for (int i = 0; i < temp.size(); i++) {
+            answer[i] = temp.get(i);
+        }
+        return answer;
     }
 }
