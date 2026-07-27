a07fb29d4737498b478dc494678e09cc5a396f30
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-07-27 09:49:30 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 30.45 ms, Memory: 120 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Solution {
     public int[] solution(int[] arr) {
         // 직전 요소와 비교를 위해 Stack 사용
-            // 자료구조 초기화
+        // 자료구조 초기화
         Deque<Integer> stack = new ArrayDeque<>();
         List<Integer> answer = new ArrayList<>();
         
@@ -12,21 +12,25 @@ public class Solution {
             // Stack이 비어있다면?
             if (stack.isEmpty()) {
                 // Stack에 추가
-                stack.add(arr[i]);
+                stack.push(arr[i]);
             } else {
             // 추가하려는 요소와 직전 요소 비교
                 // 같다면?
                 if (arr[i] == stack.peek()) {
-                    continue;
+                    continue; // 무시하고 넘어가기
                 } else {
                 // 다르다면?
+		                // Stack에서 pop한 값을 정답 리스트에 즉시 추가하기
                     answer.add(stack.pop());
-                    stack.add(arr[i]);
+                    // Stack에 값 추가하기 
+                    stack.push(arr[i]); 
                 }
             }
         }
+        // 마지막까지 Stack에 남아있는 값을 정답 리스트에 추가 
         answer.add(stack.pop());
         
+        // 리스트를 배열로 변환하여 반환
         return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
