654c3f114e46a222645c91f8928f1f877f55cbb1
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-08-16 18:46:17 +0900

    [level 2] Title: 소수 찾기, Time: 16.89 ms, Memory: 92.4 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,49 +1,18 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
-    private boolean[] isPrime;
-    private Set<Integer> possibleCombinations = new HashSet<>();
-    private List<String> possibleChoices = new ArrayList<>();
-    private int maxLength = -1;
-    private boolean[] used;
-
     public int solution(String numbers) {
-        // 주어진 문자열로부터 도출할 수 있는 최대 숫자 구하기
-        String[] arr = numbers.split("");
-        System.out.println("unsorted : " + Arrays.toString(arr));
-        Arrays.sort(arr, (a, b) -> b.compareTo(a));
-        System.out.println("sorted : " + Arrays.toString(arr));
-        int max = Integer.parseInt(String.join("", arr));
-        System.out.println("The possible max number is " + max + "\n------------------");
-        
-        // [에라토스테네스의 체]를 통해 주어진 범위(0 ~ 최대 숫자) 내의 모든 소수 구하기
-        this.isPrime = new boolean[max + 1];
-        Arrays.fill(this.isPrime, true);
-        this.isPrime = sieveOfEratothenes(this.isPrime, max);
-
         // 주어진 문자열로부터 도출할 수 있는 모든 조합 구하기
-        for (String num : arr) {
-            this.possibleChoices.add(num);
-        }
-        this.maxLength = numbers.length();
-        List<String> state = new ArrayList<>();
-        this.used = new boolean[possibleChoices.size()];
-        backtracking(state);
+        Set<Integer> possibleCombinations = new HashSet<>();
+        String[] possibleChoices = numbers.split("");
+        String currentPath = "";
+        boolean[] used = new boolean[possibleChoices.length];
+        backtracking(possibleCombinations, possibleChoices, used, currentPath);
 
         // 각 조합이 소수인지 여부 집계하기
-        System.out.println("----------------------\n이제 소수인지 여부를 확인해보겠습니다!\n---------------------------");
         int count = 0;
-        int sizeOfSet = possibleCombinations.size();
-        System.out.println("현재 Set에는 몇 개? : " + sizeOfSet);
         for (Integer combination : possibleCombinations) {
-            System.out.println(combination + "은 소수일까요?");
-
-            if (isPrime[combination]) {
-                System.out.println(combination + "은 소수입니다!");
+            if (isPrime(combination)) {
                 count++;
             }
         }
@@ -51,42 +20,38 @@ class Solution {
         return count;
     }
 
-    private boolean[] sieveOfEratothenes(boolean[] isPrime, int max) {
-        isPrime[0] = false;
-        isPrime[1] = false;
+    private boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
 
-        for (int p = 2; p * p <= max; p++) {
-            for (int i = p * p; i <= max; i += p) {
-                isPrime[i] = false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
             }
         }
 
-        return isPrime;
+        return true;
     }
 
-    private void backtracking(List<String> state) {
-        if (state != null && !state.isEmpty()) {
-            String combinationString = String.join("", new ArrayList<>(state));
-            int combination = Integer.parseInt(combinationString);
+    private void backtracking(Set<Integer> possibleCombinations, String[] possibleChoices, boolean[] used, String currentPath) {
+        if (currentPath != null && !currentPath.isEmpty()) {
+            int combination = Integer.parseInt(currentPath);
             possibleCombinations.add(combination);
             
             // bounding function
-            if (state.size() == maxLength) {
+            if (currentPath.length() == possibleChoices.length) {
                 return;
             }
         }
 
         // building the tree
-        for (int i = 0; i < possibleChoices.size(); i++) {
-            if (used[i]) {
-                continue;
+        for (int i = 0; i < possibleChoices.length; i++) {
+            if (!used[i]) {
+                used[i] = true;
+                backtracking(possibleCombinations, possibleChoices, used, currentPath + possibleChoices[i]); // using the recursion
+                used[i] = false;                
             }
-
-            state.add(possibleChoices.get(i)); // making the branch
-            used[i] = true;
-            backtracking(state); // using the recursion
-            state.remove(state.size() - 1); // going back by popping
-            used[i] = false;
         }
     }
 }
