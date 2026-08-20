4a2df57025a2510b8df067b41393e7c3ed30c2c6
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-08-21 08:29:29 +0900

    [level 2] Title: 큰 수 만들기, Time: 16.39 ms, Memory: 91.9 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -4,17 +4,17 @@ class Solution {
         int len = number.length();
 
         for (int i = 0; i < len; i++) {
-            char current = number.charAt(i);
-
-            // 스택의 top이 현재 문자보다 작고 k가 남아있다면 제거
+            char current = number.charAt(i); // Stack 역할
+            
+            // 가장 왼쪽 문자(StringBuilder 기준 가장 오른쪽 문자)가 현재 문자보다 작고 k가 남아있다면 제거
             while (sb.length() > 0 && sb.charAt(sb.length() - 1) < current && k > 0) {
                 sb.deleteCharAt(sb.length() - 1);
                 k--;
             }
             sb.append(current);
         }
-
-        // k가 남아있는 경우 뒤에서 남은 k만큼 제거 (예: "7777", k=2 -> "77")
+        
+        // k가 남아있는 경우 뒤에서 남은 k만큼 제거
         return sb.substring(0, sb.length() - k);
     }
 }
