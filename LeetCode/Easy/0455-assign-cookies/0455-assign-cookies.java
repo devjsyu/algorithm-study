9bf2bd8a7c310850d4f958b6fea5590914f89f42
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-08-21 16:33:11 +0900

    Time: 14 ms (91.92%), Space: 51.3 MB (74.98%) - LeetHub

--- LeetCode/Easy/0455-assign-cookies/0455-assign-cookies.java
+++ LeetCode/Easy/0455-assign-cookies/0455-assign-cookies.java
@@ -6,19 +6,16 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
 
-        // 오름차순으로 정렬된 모든 쿠키 순회하기
-        int j = 0;
-        int count = 0;
-        for (int i = 0; i < s.length; i++) {
-            if (j >= g.length) break;
-            
-            // 만족하는 경우
-            if (s[i] >= g[j]) {
-                j++;
-                count++;
-            } 
+        // Two Pointer 사용하여 O(N) 시간복잡도로 순회하기
+        int contentChildren = 0;
+        int cookieIndex = 0;
+        while (cookieIndex < s.length && contentChildren < g.length) {
+            if (s[cookieIndex] >= g[contentChildren]) {
+                contentChildren++;
+            }
+            cookieIndex++;
         }
 
-        return count;
+        return contentChildren;
     }
 }
