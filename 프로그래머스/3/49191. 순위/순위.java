6b0d4b16768d4b2681071ed05963c1830c6e0de8
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-06 15:00:41 +0900

    [level 3] Title: 순위, Time: 13.55 ms, Memory: 89.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,35 +1,40 @@
+/**
+- 플로이드 워셜 : directional graph 1개를 두 가지 방법으로 해석
+- BFS : directional graph 2개를 각각 winGraph, loseGraph로 해석
+    - BFS에서 adjacencyList는 out-edge만을 저장하기 때문이다. 
+    
+- 주어진 배열을 두 번 순회하면서 winGraph와 loseGraph를 초기화한다.    
+- 각 노드를 순회하면서 winGraph, loseGraph에서 각각 확실히 알 수 있는 것을 누적 집계한다.
+- 누적 집계된 값이 n - 1이라면 모든 승패를 알 수 있게 된다.
+- 해당 경우의 수를 누적 집계하여 반환한다.
+*/
 import java.util.*;
 
-/*
-승패 관계를 방향이 있는 그래프로 모델링하고,
-탐색을 통해 관계가 명확한 노드의 수를 세기
- */
 class Solution {
     public int solution(int n, int[][] results) {
         // 전체 인접 리스트 초기화
         List<List<Integer>> winGraph = new ArrayList<>();
         List<List<Integer>> loseGraph = new ArrayList<>();
-
+        
         // 각 노드에 대한 인접 리스트 초기화
         for (int i = 0; i <= n; i++) {
             winGraph.add(new ArrayList<>());
             loseGraph.add(new ArrayList<>());
         }
-
+        
         // 모든 경기 결과 순회하면서 전체 인접 리스트 업데이트
-        for (int[] r : results) {
-            winGraph.get(r[0]).add(r[1]);
-            loseGraph.get(r[1]).add(r[0]);
+        for (int[] result : results) {
+            winGraph.get(result[0]).add(result[1]);
+            loseGraph.get(result[1]).add(result[0]);
         }
-
+        
         // 모든 노드에 대해 순회
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            // 각 노드별 이긴 횟수 집계
+            // 각 노드별 이기고 진 횟수 집계
             int winCount = bfs(i, winGraph, n);
-            // 각 노드별 진 횟수 집계
             int loseCount = bfs(i, loseGraph, n);
-
+            
             // 이긴 횟수와 진 횟수의 합이 n - 1이 되는 경우를 집계
             if (winCount + loseCount == n - 1) {
                 answer++;
@@ -38,27 +43,27 @@ class Solution {
 
         return answer;
     }
-
+    
     private int bfs(int start, List<List<Integer>> graph, int n) {
         boolean[] visited = new boolean[n + 1];
         Queue<Integer> queue = new ArrayDeque<>();
-
+        
         queue.offer(start);
         visited[start] = true;
         int count = 0;
-
+        
         while (!queue.isEmpty()) {
             int curr = queue.poll();
-
+            
             for (int next : graph.get(curr)) {
                 if (!visited[next]) {
                     visited[next] = true;
-                    count++; // 이기거나 진 횟수 업데이트
+                    count++;
                     queue.offer(next);
                 }
             }
         }
-
+        
         return count;
     }
 }
