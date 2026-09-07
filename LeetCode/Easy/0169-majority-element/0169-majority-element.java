7422c59ec20d3680ad3eb2d688537da3a04be180
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-07 19:23:08 +0900

    Time: 1 ms (99.9%), Space: 55.7 MB (31.67%) - LeetHub

---
+++ LeetCode/Easy/0169-majority-element/0169-majority-element.java
@@ -0,0 +1,20 @@
+// Boyer Moore 과반수 투표 알고리즘
+class Solution {
+    public int majorityElement(int[] nums) {
+        int candidate = nums[0];
+        int count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+
+            if (num == candidate) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+        return candidate;
+    }
+}
