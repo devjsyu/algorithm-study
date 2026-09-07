7ef9da58bff5ee7489895e2e6218a905c73095cc
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-07 15:37:18 +0900

    [level 4] Title: 징검다리, Time: 19.51 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,41 +1,30 @@
+/**
+최적화 문제를 단조성 있는 결정 문제로 전환하여 이분 탐색으로 풀기
+- AS-IS: 특정 조건을 만족하는 최댓값은 무엇인가?
+- TO-BE: "x일 때 특정 조건을 만족하는가?" 결정 함수에 대한 Rightmost True 찾기
+
+- 거리의 최솟값이 x일 때, 제거해야 하는 바위가 n이다. n이 주어졌을 때, 조건을 만족하는 x의 최댓값은?
+*/
+
 import java.util.*;
 
 class Solution {
-    /**
-    - 최적화 문제를 결정 문제로 전환하여 Parametric Search 사용
-        - 최적화 문제 (각 지점 사이의 거리의 최솟값 중 가장 큰 값을 구하시오)
-        - 결정 문제 (각 지점 사이의 거리, x가 바위 n개 제거로 가능한가?)
-        
-    - 결정 문제
-    x가 1일 때 바위 n개 제거로 가능?
-    x가 2일 때 바위 n개 제거로 가능?
-    ...
-    x가 1,000,000,000일 때 바위 n개 제거로 가능?
-    
-    - upper bound
-    x가 x'일 때 바위 n개 이하로 제거 가능? -> x'보다 더 커도 된다
-    x가 x'일 때 바위 n개 초과로 제거 가능? -> x'보다 더 작아야 한다
-    
-    - Monotonic
-    x가 커질수록 판별함수의 값도 커진다
-    (당연히 각 지점 사이의 거리를 늘리기 위해서는 제거해야 하는 바위 수가 많아진다)
-    */
     public int solution(int distance, int[] rocks, int n) {
-        // rocks 배열 오름차순 정렬
+        // 주어진 배열 오름차순 정렬
         Arrays.sort(rocks);
         
-        // 변수 초기화
-        int left = 1; // 각 지점 사이의 거리 최솟값
-        int right = distance; // 각 지점 사이의 거리 최댓값
-        int answer = 0;
+        // x의 범위 초기화
+        int left = 1;
+        int right = distance;
         
-        // Parametric Search with iterative approach 
+        // 이분탐색
+        int answer = 0;
         while (left <= right) {
             int middle = (left + right) / 2;
             
-            int result = check(distance, rocks, middle);
-            if (n >= result) {
-                answer = middle;
+            // Rightmost True 찾기
+            if (n >= check(middle, rocks, distance)) {
+                answer = middle; // 정답 갱신
                 left = middle + 1;
             } else {
                 right = middle - 1;
@@ -45,23 +34,28 @@ class Solution {
         return answer;
     }
     
-    // 각 지점 사이의 거리 : x
-    // 제거해야 하는 바위 개수 : 반환값
-    private int check(int distance, int[] rocks, int x) {
+    // Greedy algorithm
+    // 거리의 최솟값 x를 만족시키기 위해 제거해야 하는 바위의 개수 반환
+    private int check(int x, int[] rocks, int distance) {
+        // 출발지점 초기화
+        int curr = 0;
+
+        // 바위 순회하며 x를 만족하는지 조사
         int rockCount = 0;
-        int current = 0;
-        
         for (int rock : rocks) {
-            if (rock - current >= x) {
-                current = rock;
-            } else {
+            if (rock - curr < x) {
+                // 기존 curr 유지하면서 바위 누적 집계하기
                 rockCount++;
-            }    
+            } else {
+                // curr 갱신하기
+                curr = rock;
+            }
         }
         
-        if (distance - current < x) {
+        // 도착지점 조사
+        if (distance - curr < x) {
             rockCount++;
-        } 
+        }
         
         return rockCount;
     }
