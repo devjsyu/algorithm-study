3bf976eaa7782fb9365c9497e88a7d348e7c7210
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-07 12:44:25 +0900

    [level 3] Title: 순위, Time: 9.52 ms, Memory: 90.1 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,65 +1,67 @@
 /**
-- 플로이드 워셜 : directional graph 1개를 두 가지 방법으로 해석
-- BFS : directional graph 2개를 각각 winGraph, loseGraph로 해석
-    - BFS에서 adjacencyList는 out-edge만을 저장하기 때문이다. 
-    
-- 주어진 배열을 두 번 순회하면서 winGraph와 loseGraph를 초기화한다.    
-- 각 노드를 순회하면서 winGraph, loseGraph에서 각각 확실히 알 수 있는 것을 누적 집계한다.
-- 누적 집계된 값이 n - 1이라면 모든 승패를 알 수 있게 된다.
-- 해당 경우의 수를 누적 집계하여 반환한다.
+정확하게 순위를 매길 수 있는 조건
+- 이긴 횟수 + 진 횟수 = n - 1
+
+선수와 선수 간 이기고 진 것에 대한 관계는 그래프로 나타낼 수 있다.
+A 선수가 B 선수를 이겼다에 대한 해석은 directional graph일 때 가능하다.
+일반적인 HashMap 형태로 구현된 directional graph에서는 out node만 저장하기 때문에, 
+한 그래프를 두 가지 방식으로 해석하기 어렵다. 
+따라서 이긴 관계에 대한 그래프 따로, 진 관계에 대한 그래프 따로 만들어야 한다. 
+
+각 선수에 대해 winGraph와 loseGraph에서 각 선수로부터 출발하는 BFS 탐색을 하면,
+해당 선수의 이긴 횟수와 진 횟수를 알 수 있다.
 */
+
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] results) {
-        // 전체 인접 리스트 초기화
+        // 그래프 초기화
         List<List<Integer>> winGraph = new ArrayList<>();
-        List<List<Integer>> loseGraph = new ArrayList<>();
-        
-        // 각 노드에 대한 인접 리스트 초기화
         for (int i = 0; i <= n; i++) {
             winGraph.add(new ArrayList<>());
+        }
+        
+        List<List<Integer>> loseGraph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
             loseGraph.add(new ArrayList<>());
         }
         
-        // 모든 경기 결과 순회하면서 전체 인접 리스트 업데이트
         for (int[] result : results) {
             winGraph.get(result[0]).add(result[1]);
             loseGraph.get(result[1]).add(result[0]);
         }
         
-        // 모든 노드에 대해 순회
         int answer = 0;
         for (int i = 1; i <= n; i++) {
-            // 각 노드별 이기고 진 횟수 집계
             int winCount = bfs(i, winGraph, n);
             int loseCount = bfs(i, loseGraph, n);
             
-            // 이긴 횟수와 진 횟수의 합이 n - 1이 되는 경우를 집계
             if (winCount + loseCount == n - 1) {
-                answer++;
+                answer++;    
             }
         }
-
+        
         return answer;
     }
     
-    private int bfs(int start, List<List<Integer>> graph, int n) {
-        boolean[] visited = new boolean[n + 1];
+    // 인자로 주어진 해당 선수가 이기거나 진 횟수를 반한
+    private int bfs(int player, List<List<Integer>> graph, int n) {
         Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1];
         
-        queue.offer(start);
-        visited[start] = true;
+        queue.add(player);
+        visited[player]= true;
+        
         int count = 0;
-        
         while (!queue.isEmpty()) {
             int curr = queue.poll();
             
-            for (int next : graph.get(curr)) {
-                if (!visited[next]) {
-                    visited[next] = true;
+            for (int neighbor : graph.get(curr)) {
+                if (!visited[neighbor]) {
+                    queue.add(neighbor);
+                    visited[neighbor] = true;
                     count++;
-                    queue.offer(next);
                 }
             }
         }
