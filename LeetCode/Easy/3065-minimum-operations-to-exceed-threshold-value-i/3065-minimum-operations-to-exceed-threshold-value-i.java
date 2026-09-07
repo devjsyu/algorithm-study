8b959b18a7bfd468a614a1fbe93065ff3bfe1227
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-07 18:20:31 +0900

    Time: 1 ms (39.21%), Space: 44.8 MB (28.85%) - LeetHub

--- LeetCode/Easy/3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.java
+++ LeetCode/Easy/3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.java
@@ -1,24 +1,11 @@
-import java.util.*;
-
-// Lower Bound
 class Solution {
     public int minOperations(int[] nums, int k) {
-        // 오름차순 정렬
-        Arrays.sort(nums);
-
-        int left = 0;
-        int right = nums.length; // 주어진 배열 속 모든 원소가 조건 맞지 않는 경우 대비(주어진 배열 속 모든 원소가 조건에 맞지 않는 경우라면, left가 nums.length - 1이 된다)
-
-        while (left < right) {
-            int mid = left + (right - left) / 2;
-
-            if (k <= nums[mid]) {
-                right = mid;
-            } else {
-                left = mid + 1;
+        int count = 0;
+        for (int num : nums) {
+            if (num < k) {
+                count++;
             }
         }
-
-        return left;
+        return count;
     }
 }
