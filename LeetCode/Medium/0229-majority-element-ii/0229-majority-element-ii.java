c5f99aa3ed91bbe718b6f4bbce9f969714a18bde
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-09 21:22:33 +0900

    Time: 2 ms (99.95%), Space: 52.7 MB (40.89%) - LeetHub

--- LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
+++ LeetCode/Medium/0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,35 +1,31 @@
 /**
-- Boyer Moore's Majority Vote algorithm을 활용하기
-    - n / 3보다 큰 득표를 한 사람은 최대 2명일 수밖에 없다!
-
-- 기존 Boyer Moore's Majority Vote algorithm 방식에서 변형
-    - AS-IS: candidate, count
-    - TO-BE: candidate1, count1, candidate2, count2
-
-- 이전과 동일: count가 0이 될 때 candidate 교체하기
-- 이번에 달라진 점: candidate 외 다른 사람 등장하면 count1, count2 동시 차감하기
+Boyer Moore Majority vote algorithm
+최대 2명이 n / 3 이상 득표 가능
  */
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        Integer candidate1 = null;
-        Integer candidate2 = null;
-        int count1 = 0; 
+        int candidate1 = 0;
+        int candidate2 = 0;
+        int count1 = 0;
         int count2 = 0;
-
+        
         for (int num : nums) {
-            if (candidate1 != null && num == candidate1) {
+            // candidate 득표
+            if (candidate1 == num) {
                 count1++;
-            } else if (candidate2 != null && num == candidate2) {
+            } else if (candidate2 == num) {
                 count2++;
+            // candidate 초기화
+            // candidate의 count가 0이 된 경우
             } else if (count1 == 0) {
                 candidate1 = num;
                 count1 = 1;
             } else if (count2 == 0) {
                 candidate2 = num;
-                count2 = 1;
-            } else {
-                // 세 숫자가 모두 다르면 두 후보의 카운트를 1씩 차감
+                count2 = 1;                
+            // candidate1, 2 외 다른 사람이 득표할 경우
+            } else if (candidate1 != num && candidate2 != num) {
                 count1--;
                 count2--;
             }
@@ -38,21 +34,17 @@ class Solution {
         count1 = 0;
         count2 = 0;
         for (int num : nums) {
-            if (candidate1 != null && candidate1 == num) {
+            if (candidate1 == num) {
                 count1++;
-            } else if (candidate2 != null && candidate2 == num) {
+            } else if (candidate2 == num) {
                 count2++;
             }
         }
-        int threshold = nums.length / 3;
 
+        int threshold = nums.length / 3;
         List<Integer> answer = new ArrayList<>();
-        if (count1 > threshold) {
-            answer.add(candidate1);
-        }
-        if (count2 > threshold) {
-            answer.add(candidate2);
-        }
+        if (count1 > threshold) answer.add(candidate1);
+        if (count2 > threshold) answer.add(candidate2);
 
         return answer;
     }
