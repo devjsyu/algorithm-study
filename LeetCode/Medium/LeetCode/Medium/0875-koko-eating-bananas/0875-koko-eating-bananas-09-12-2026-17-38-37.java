8618fee666f004e25381a16eb1e08a0cd711fc67
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-12 17:38:50 +0900

    09-12-2026 0875-koko-eating-bananas 7 ms (83.23%) 48.1 MB (22.61%)

---
+++ LeetCode/Medium/LeetCode/Medium/0875-koko-eating-bananas/0875-koko-eating-bananas-09-12-2026-17-38-37.java
@@ -0,0 +1,33 @@
+// 최적화 문제를 단조성을 띈 결정 문제로 전환해 이분탐색 하기
+// 속도 k가 커질수록, 시간 f(k)이 작아진다
+// Leftmost True 구하기
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int left = 1;
+        int right = 1_000_000_000;
+        int answer = 0;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (h >= check(mid, piles)) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return answer;
+    }
+
+    private long check(int x, int[] piles) {
+        long time = 0;
+
+        for (int pile : piles) {
+            time += (long) (pile + (x - 1)) / x; // ceil 연산
+        }
+
+        return time;
+    }
+}
