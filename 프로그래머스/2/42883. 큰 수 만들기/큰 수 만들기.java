d45efdbae1b5f13725744bb29965aca3785a5294
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-12 17:10:27 +0900

    [level 2] Title: 큰 수 만들기, Time: 21.91 ms, Memory: 96.4 MB -BaekjoonHub

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.java
@@ -1,20 +1,51 @@
+/**
+점점 커지는 양상이라면? 일단 k가 허락하는 한, 계속 없애기, 다음 것이 더 작다면 없애기 중단하기
+점점 줄어드는 양상이라면? 그러다가 새로 더 큰 게 나왔다면? k가 허락하는 한, 계속 없애기
+확정되었다면, 다음 것부터 다시 시작하기
+
+// Monotonic Stack
+// 초기화
+// top과 새로 들어온 값 비교하기
+    // 새로 들어온 값이 더 크다?
+        // while
+            // k가 허락하는 한 없애기
+            // stack.pop
+        // stack.push
+    // 새로 들어온 값이 더 작다?
+        // stack.push
+*/
 class Solution {
     public String solution(String number, int k) {
+        // 바로 정답으로 반환할 수 있도록 StringBuilder를 Stack처럼 사용하기
         StringBuilder sb = new StringBuilder();
-        int len = number.length();
-
-        for (int i = 0; i < len; i++) {
-            char current = number.charAt(i); // Stack 역할
+        sb.append(number.charAt(0));
+        
+        // number 문자열 순회하는 과정 중 제거한 숫자가 k가 될 때 중단
+        int removed = 0;
+        for (int i = 1; i < number.length(); i++) {
+            char current = number.charAt(i);
+            char top = sb.charAt(sb.length() - 1);
             
-            // 가장 왼쪽 문자(StringBuilder 기준 가장 오른쪽 문자)가 현재 문자보다 작고 k가 남아있다면 제거
-            while (sb.length() > 0 && sb.charAt(sb.length() - 1) < current && k > 0) {
-                sb.deleteCharAt(sb.length() - 1);
-                k--;
-            }
-            sb.append(current);
+            if (top < current) {
+                while (removed < k && top < current) {
+                    removed++;
+                    sb.deleteCharAt(sb.length() - 1);
+                    
+                    if (sb.length() == 0) break;
+                    top = sb.charAt(sb.length() - 1);
+                }
+                sb.append(current);
+            } else {
+                sb.append(current);
+            }            
         }
         
-        // k가 남아있는 경우 뒤에서 남은 k만큼 제거
-        return sb.substring(0, sb.length() - k);
+        // 제거한 수가 아직 k에 못 미칠 경우
+        int rest = k - removed;
+        int length = sb.length();
+        sb = sb.delete(length - rest, length);
+        
+        String answer = sb.toString();
+        return answer;
     }
 }
