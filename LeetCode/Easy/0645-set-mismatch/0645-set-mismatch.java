21b1eced782cf34b50483f6617720431159025f6
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-20 16:55:03 +0900

    Time: 2 ms (95.09%), Space: 45.7 MB (99.87%) - LeetHub

--- LeetCode/Easy/0645-set-mismatch/0645-set-mismatch.java
+++ LeetCode/Easy/0645-set-mismatch/0645-set-mismatch.java
@@ -1,20 +1,22 @@
+import java.util.*;
+
 class Solution {
     public int[] findErrorNums(int[] nums) {
-        int[] answer = new int[2];
-        Map<Integer, Integer> numberToCount = new HashMap<>();
+        int[] count = new int[nums.length + 1];
 
         for (int i = 0; i < nums.length; i++) {
-            numberToCount.put(nums[i], numberToCount.getOrDefault(nums[i], 0) + 1);
+            count[nums[i]]++;
         }
 
+        int[] answer = new int[2];
         for (int i = 1; i <= nums.length; i++) {
-            if (!numberToCount.containsKey(i)) {
-                answer[1] = i;
-            } else if (numberToCount.get(i) == 2) {
+            if (count[i] == 2) {
                 answer[0] = i;
+            } else if (count[i] ==0) {
+                answer[1] = i;
             }
         }
-        
+
         return answer;
     }
 }
