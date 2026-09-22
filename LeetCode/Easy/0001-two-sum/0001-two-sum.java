d84d3c5a9a11c50405917709399762fdb1b123ce
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-22 13:43:35 +0900

    09-22-2026 0001-two-sum 4 ms (51.86%) 46.6 MB (93.52%)

--- LeetCode/Easy/0001-two-sum/0001-two-sum.java
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.java
@@ -1,12 +1,18 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    return new int[]{i, j};
-                }
+        Map<Integer, Integer> numberToIndex = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            numberToIndex.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+            if (numberToIndex.containsKey(rest) && numberToIndex.get(rest) != i) {
+                return new int[]{i, numberToIndex.get(rest)};
             }
         }
+
         return null;
     }
 }
