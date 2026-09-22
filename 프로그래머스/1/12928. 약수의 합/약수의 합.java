e48e25b2f7b83387af4034863ba522ffe0e16c00
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-23 07:24:16 +0900

    [level 1] Title: 약수의 합, Time: 0.05 ms, Memory: 80.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        int sqrt = (int) Math.sqrt(n);
+        for (int i = 1; i <= sqrt; i++) {
+            if (n % i == 0) {
+                answer += i;
+                
+                if (i != sqrt) {
+                    answer += n / i;                    
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
