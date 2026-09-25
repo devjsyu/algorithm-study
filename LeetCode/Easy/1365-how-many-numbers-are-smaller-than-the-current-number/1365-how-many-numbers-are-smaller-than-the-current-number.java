30cfa944320dfb50a752a185f4bba504b901e312
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-25 11:18:30 +0900

    Time: 20 ms (5.24%), Space: 47 MB (5.84%) - LeetHub

--- LeetCode/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ LeetCode/Easy/1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,14 +1,16 @@
+// 주어진 배열로 리스트를 초기화 한다
+// 리스트를 오름차순으로 정렬한다
+// 배열의 각 원소에 대해 리스트의 indexOf 메서드 호출한다
+import java.util.*;
+
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        List<Integer> ordered = new ArrayList<>();
-        for (int num : nums) {
-            ordered.add(num);
-        }
-        Collections.sort(ordered);
+        List<Integer> list = Arrays.stream(nums).sorted().boxed().collect(Collectors.toList());
 
         int[] answer = new int[nums.length];
+
         for (int i = 0; i < nums.length; i++) {
-            answer[i] = ordered.indexOf(nums[i]);
+            answer[i] = list.indexOf(nums[i]);
         }
 
         return answer;
