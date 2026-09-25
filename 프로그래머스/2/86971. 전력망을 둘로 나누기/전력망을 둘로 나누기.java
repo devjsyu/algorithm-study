3da31eaf9514782b5fdffc1abba1665f14f6725c
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-25 10:59:24 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.60 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,66 +1,73 @@
+// tree : connected, acyclic, undirected graph
+// 현재 그래프는 tree이지만, root node가 지정되지 않았고, rooted tree는 아니다.
+// 총 전선 개수는 고정되어 있기 때문에, 한쪽만 구하면 나머지 한쪽은 나머지로서 구할 수 있다.
 import java.util.*;
 
 class Solution {
     public int solution(int n, int[][] wires) {
-        this.n = n;
-        this.adjacencyList = new ArrayList[n + 1]; // 1-indexed
+        // 그래프 초기화
+        List<Integer>[] graph = new ArrayList[n + 1]; // 1-index
+        
         for (int i = 1; i <= n; i++) {
-            adjacencyList[i] = new ArrayList<>();
+            graph[i] = new ArrayList<>();
         }
         
-        for (int i = 0; i < wires.length; i++) {
-            addEdge(wires[i][0], wires[i][1]);
+        for (int[] wire : wires) {
+            int v1 = wire[0];
+            int v2 = wire[1];
+            
+            graph[v1].add(v2);
+            graph[v2].add(v1);
         }
         
         int min = Integer.MAX_VALUE;
-        int abs;
-        for (int i = 0; i < wires.length; i++) {
-            // 끊기
-            adjacencyList[wires[i][0]].remove(Integer.valueOf(wires[i][1]));
-            adjacencyList[wires[i][1]].remove(Integer.valueOf(wires[i][0]));            
-
-            int count = dfs(i + 1); // 1-index
+        
+        // wires 배열 순회
+        // 하나씩 끊고 복구하면서 완전탐색
+        for (int[] wire : wires) {
+            int v1 = wire[0];
+            int v2 = wire[1];
             
-            abs = Math.abs(n - count - count);
-            if (min > abs) {
-                min = abs;
-            }
+            // 기존 그래프에서 특정 전선 끊어보기
+            graph[v1].remove(Integer.valueOf(v2));
+            graph[v2].remove(Integer.valueOf(v1));
             
-            // 복구
-            addEdge(wires[i][0], wires[i][1]);
+            // BFS로 그래프 순회하면서 해당 그래프 속 시작 노드와 연결된 모든 노드 개수 반환
+            int nodeCountGraph1 = bfs(n, graph, v1);
+            int nodeCountGraph2 = n - nodeCountGraph1;
+                        
+            // 최솟값과 비교 및 갱신
+            min = Math.min(Math.abs(nodeCountGraph2 - nodeCountGraph1), min);
+            
+            // 끊었던 전선 복구하기
+            graph[v1].add(v2);
+            graph[v2].add(v1);
         }
-
+        
         return min;
     }
     
-    private int n;
-    private List<Integer>[] adjacencyList;    
-    
-    private void addEdge(int v1, int v2) {
-        adjacencyList[v1].add(v2);
-        adjacencyList[v2].add(v1);
-    }
-    
-    // 해당 노드를 부모 노드로 하는 그래프의 총 노드 개수 반환
-    private int dfs(int start) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        boolean[] visited = new boolean[n + 1];
+    private int bfs(int n, List<Integer>[] graph, int start) {
+        Queue<Integer> queue = new ArrayDeque<>();
+        boolean[] visited = new boolean[n + 1]; // 1-index
         int count = 0;
         
-        stack.push(start);
+        queue.offer(start);
         visited[start] = true;
         count++;
         
-        while (!stack.isEmpty()) {
-            int current = stack.pop();
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
             
-            for (int next : adjacencyList[current]) {
-                if (!visited[next]) {
-                    stack.push(next);
-                    visited[next] = true;
-                    count++;
-                } 
-            }
+            for (int neighbor : graph[current]) {
+                if (visited[neighbor]) {
+                    continue;
+                }
+                
+                queue.offer(neighbor);
+                visited[neighbor] = true;
+                count++;
+            }    
         }
         
         return count;
