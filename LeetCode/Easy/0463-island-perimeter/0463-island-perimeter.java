5eb20cacf6926b76d8edabdab1f5593ca6e31849
jaesungyu <149031461+devjsyu@users.noreply.github.com>
2026-09-26 13:07:43 +0900

    Time: 10 ms (7.39%), Space: 47.6 MB (16.3%) - LeetHub

--- LeetCode/Easy/0463-island-perimeter/0463-island-perimeter.java
+++ LeetCode/Easy/0463-island-perimeter/0463-island-perimeter.java
@@ -2,40 +2,36 @@ class Solution {
     public int islandPerimeter(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
+        
+        // 시작점 찾기
+        int[] start = findStartPoint(grid, rows, cols);
 
-        int[] startPosition = findStartPosition(grid, rows, cols);
-
-        return bfs(grid, rows, cols, startPosition);
-    }
-
-    private int[] findStartPosition(int[][] grid, int rows, int cols) {
-        // 최초로 grid[i][j] == 1인 지점 찾아서 BFS 시작하기
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (grid[i][j] == 1) {
-                    return new int[]{i, j};
-                } 
-            }
-        }
-        return null;
+        return bfs(grid, rows, cols, start);
     }
 
     // 상하좌우
     private static final int[] dr = {-1, 1, 0, 0};
     private static final int[] dc = {0, 0, -1, 1};
 
-    private int bfs(int[][] grid, int rows, int cols, int[] coordinates) {
+    private int[] findStartPoint(int[][] grid, int rows, int cols) {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == 1) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private int bfs(int[][] grid, int rows, int cols, int[] start) {
+        Queue<int[]> queue = new ArrayDeque<>();
+        boolean[][] visited = new boolean[rows][cols];
         int count = 0;
 
-        Queue<int[]> queue = new ArrayDeque<>();
-
-        boolean[][] visited = new boolean[rows][cols];
-
-        int row = coordinates[0];
-        int col = coordinates[1];
-
-        queue.offer(coordinates);
-        visited[row][col] = true;
+        queue.offer(start);
+        visited[start[0]][start[1]] = true;
 
         while (!queue.isEmpty()) {
             int[] current = queue.poll();
@@ -45,7 +41,7 @@ class Solution {
                 int nc = current[1] + dc[d];
 
                 if (nr < 0 || nr >= rows || nc < 0 || nc >= cols) {
-                    count++; // 외곽과 맞닿으면 노란선 조건 만족
+                    count++;
                     continue;
                 }
 
@@ -54,15 +50,13 @@ class Solution {
                 }
 
                 if (grid[nr][nc] == 0) {
-                    count++; // 0과 맞닿으면 노란선 조건 만족
+                    count++;
                     continue;
                 }
 
-                // from now on, this is quaranteed to be 
-                // on-grid, not visited, tile
                 queue.offer(new int[]{nr, nc});
                 visited[nr][nc] = true;
-            } 
+            }
         }
 
         return count;
